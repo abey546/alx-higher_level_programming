59e4c0ee172d23c49876a169969af70681570733
abey546 <abeyhassan546@gmail.com>
2023-10-03 06:00:53 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123)
-if char(i) not in 'qe':
-     print("{}".format(chr(letter)), end="")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
