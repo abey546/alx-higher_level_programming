abeb7d2431be443f53301fe8123466a57537ceb5
abey546 <abeyhassan546@gmail.com>
2023-10-23 08:49:36 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,18 @@
-i#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    Return
+#!/usr/bin/python3
+"""to print x element of a list"""
 
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for a in range(0, x):
+            print(f"{my_list[a]}", end="")
+        print()
+
+        return (x)
+    except IndexError:
+        b = 0
+        for a in my_list:
+            b = b + 1
+        print()
+
+        return (b)
