64ac7e00b3c920404604fed31908e7e07caede2a
abey546 <abeyhassan546@gmail.com>
2024-03-18 05:00:07 -0700

    updated

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""
-This script prints the first State object
-from the database `hbtn_0e_6_usa`.
-"""
 
+"""
+Module that connects python script to a database
+"""
 from sys import argv
-from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
 
-    engine = create_engine(db_url)
-    Session = sessionmaker(bind=engine)
+    my_session_maker = sessionmaker(bind=engine)
+    my_session = my_session_maker()
 
-    session = Session()
-
-    state = session.query(State).filter(State.name == argv[4]).first()
-    if state is not None:
-        print('{0}'.format(state.id))
+    for state in my_session.query(State):
+        if argv[4] == state.name:
+            print("{}".format(state.id))
+            break
     else:
         print("Not found")
+
+    my_session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""
-This script lists all State objects
-that contain the letter `a`
-from the database `hbtn_0e_6_usa`.
-"""
 
+"""
+Module that connects python script to a database
+"""
 from sys import argv
-from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
 
-    engine = create_engine(db_url)
-    Session = sessionmaker(bind=engine)
+    my_session_maker = sessionmaker(bind=engine)
+    my_session = my_session_maker()
 
-    session = Session()
+    for state in my_session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
 
-    states = session.query(State).filter(State.name.contains('a'))
-    if states is not None:
-        for state in states:
-            print('{0}: {1}'.format(state.id, state.name))
+    my_session.close()
