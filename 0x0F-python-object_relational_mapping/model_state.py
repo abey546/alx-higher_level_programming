5092c0ec04dfb2607e536d94216f3a6229745588
abey546 <abeyhassan546@gmail.com>
2024-03-18 04:48:12 -0700

    updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 
 """
-Module that connects a python script to the database
+Module that connects to the database
 """
-
-from sqlalchemy import Column, Integer, String, ForeignKey, null
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
 
 Base = declarative_base()
 
 
-class City(Base):
-    """Defining the City class"""
-    __tablename__ = 'cities'
+class State(Base):
+    """defining the state class"""
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
